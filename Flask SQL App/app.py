fc965582d7143cb6f9accad72b4e0050a55f422f
musawakiliML <musawakiliml@hotmail.com>
2022-10-12 14:21:08 +0100

    Added json data from sqlalchemy schema

--- Flask SQL App/app.py
+++ Flask SQL App/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, current_app
+from crypt import methods
+from flask import Flask, make_response, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import SQLAlchemySchema
+from marshmallow import fields
 
 
 app = Flask(__name__)
@@ -28,6 +31,24 @@ with app.app_context():
         def __repr__(self) -> str:
             return f'<Product {self.id}>'
     db.create_all()
+    
+class AuthorSchema(SQLAlchemySchema):
+    class Meta(SQLAlchemySchema.Meta):
+        model = Authors
+        sqla_session = db.session
+        
+    id = fields.Number(dump_only=True)
+    name = fields.String(required=True)
+    age = fields.Number(dump_only=True)
+    specialisation = fields.String(required=True)
+    
+    
+@app.route('/authors', methods=['GET'])
+def index():
+    get_authors = Authors.query.all()
+    author_schema = AuthorSchema(many=True)
+    authors = author_schema.dump(get_authors)
+    return make_response(jsonify({'authors':authors}))
 
 if __name__ == "__main__":
     app.run(debug=True)
