2f61660e6bf0949d0e44d4d1aae4636f7442f283
musawakiliML <musawakiliml@hotmail.com>
2022-10-13 21:26:35 +0100

    update

--- Flask SQL App/app.py
+++ Flask SQL App/app.py
@@ -27,13 +27,15 @@ with app.app_context():
             self.specialisation = specialisation
             self.age = age
             
-        def create(self):
-            db.session.add(self)
-            db.session.commit()
-            return self
-            
         def __repr__(self) -> str:
             return f'<Author {self.id}>'
+        
+    def create(value, Schema):
+            schema = Schema
+            
+            db.session.add(value)
+            db.session.commit()
+            return 
         
     db.create_all()
     
@@ -41,6 +43,7 @@ with app.app_context():
         class Meta(SQLAlchemySchema.Meta):
             model = Authors
             sqla_session = db.session
+            load_instances = True
             
         id = fields.Number(dump_only=True)
         name = fields.String(required=True)
@@ -49,23 +52,25 @@ with app.app_context():
     
  
  
- # get request
-@app.route('/authors', methods=['GET'])
-def get_authors():
-    get_authors = Authors.query.all()
-    author_schema = AuthorSchema(many=True)
-    authors = author_schema.dump(get_authors)
-    return make_response(jsonify({'authors':authors}))
+    # get request
+    @app.route('/authors', methods=['GET'])
+    def get_authors():
+        get_authors = Authors.query.all()
+        author_schema = AuthorSchema(many=True)
+        authors = author_schema.dump(get_authors)
+        return make_response(jsonify({'authors':authors}))
 
-# Post request
-@app.route('/authors', methods=['POST'])
-def create_author():
-    data = request.get_json()
-    author_schema = AuthorSchema()
-    author = author_schema.load(data)
-    result = author_schema.dump(author.create()).data
-    return make_response(jsonify({'authors':result}, 200))
+    # Post request
+    @app.route('/authors', methods=['POST'])
+    def create_author():
+        data = request.get_json()
+        author_schema = AuthorSchema()
+        author = author_schema.load(data)
+        #print(type(author))
+        result = author_schema.dump(create(author)).data
+        #print(type(result))
+        #print(db.session)
+        return make_response(jsonify({'authors':result}, 200))
     
 if __name__ == "__main__":
     app.run(debug=True)
-    
